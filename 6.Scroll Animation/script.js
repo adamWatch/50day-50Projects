e0ec7a3b3e007ff35a569ab4146e5288999f96f4
Adam Watchmaker <watchmaker1337@gmail.com>
2022-02-10 21:34:58 +0100

    add function drawRectangle and add addEventListener to scroll

--- 6.Scroll Animation/script.js
+++ 6.Scroll Animation/script.js
@@ -1,1 +1,33 @@
-document.addEventListener("scroll", () => {});
+const container = document.getElementById("container");
+
+const windowHeight = window.innerHeight;
+const rectangleHeight = windowHeight / 4;
+let allRectangleHeight = 0;
+console.log(rectangleHeight);
+
+function drawRectangle() {
+	const rectangle = document.createElement("div");
+	rectangle.style.height = rectangleHeight;
+	allRectangleHeight += rectangleHeight;
+	rectangle.innerText = "Box";
+	rectangle.classList.add("box");
+	container.appendChild(rectangle);
+}
+
+drawRectangle();
+drawRectangle();
+drawRectangle();
+
+document.addEventListener("scroll", () => {
+	let scrollY = window.scrollY;
+	console.log(allRectangleHeight);
+
+	console.log(scrollY);
+
+	if (
+		scrollY + windowHeight - rectangleHeight * 2 >= allRectangleHeight &&
+		scrollY < allRectangleHeight + rectangleHeight
+	) {
+		drawRectangle();
+	}
+});
