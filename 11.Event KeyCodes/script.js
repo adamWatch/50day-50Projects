746d764789474bf08e15150ad3fbf4e2c3704bce
Adam Watchmaker <watchmaker1337@gmail.com>
2022-02-16 11:11:19 +0100

    change "keyup" to"keydown"

--- 11.Event KeyCodes/script.js
+++ 11.Event KeyCodes/script.js
@@ -1,6 +1,6 @@
 const boxes = document.querySelectorAll(".box");
 
-document.addEventListener("keyup", (e) => {
+document.addEventListener("keydown", (e) => {
 	boxes[0].innerText = e.key;
 	boxes[1].innerText = e.keyCode;
 	boxes[2].innerText = e.code;
