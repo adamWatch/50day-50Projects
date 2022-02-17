b779820ee45fa1d1a149066b76afd874c3f77210
Adam Watchmaker <watchmaker1337@gmail.com>
2022-02-17 17:43:53 +0100

    add random logic

--- 13.Random Picker/script.js
+++ 13.Random Picker/script.js
@@ -3,11 +3,16 @@ const choicesContainer = document.querySelector(".choices-container");
 
 textarea.focus();
 
-let currentValue = "";
-let choices = [];
-
 textarea.addEventListener("keydown", (e) => {
 	createTags(e.target.value);
+
+	if (e.code === "Enter") {
+		setTimeout(() => {
+			e.target.value = "";
+		}, 10);
+
+		randomSelect();
+	}
 });
 
 function createTags(input) {
@@ -24,4 +29,41 @@ function createTags(input) {
 		tagEl.innerText = tag;
 		choicesContainer.appendChild(tagEl);
 	});
+}
+
+function randomSelect() {
+	const times = 30;
+
+	const interval = setInterval(() => {
+		const randomChoice = pickRandomChoice();
+
+		highlightChoice(randomChoice);
+
+		setTimeout(() => {
+			unHighlightChoice(randomChoice);
+		}, 100);
+	}, 100);
+
+	setTimeout(() => {
+		clearInterval(interval);
+
+		setTimeout(() => {
+			const randomChoice = pickRandomChoice();
+
+			highlightChoice(randomChoice);
+		}, 100);
+	}, times * 100);
+}
+
+function pickRandomChoice() {
+	const choices = document.querySelectorAll(".choice");
+	return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function highlightChoice(choice) {
+	choice.classList.add("picked");
+}
+
+function unHighlightChoice(choice) {
+	choice.classList.remove("picked");
 }
