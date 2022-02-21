0b0f779fc23c93221880b26f20d75242f5276f54
Adam Watchmaker <watchmaker1337@gmail.com>
2022-02-21 16:35:57 +0100

    add function which fetch api date and add to site

---
+++ 17.Movie App/script.js
@@ -0,0 +1,59 @@
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const movie = document.querySelector('.movie');
+const container = document.querySelector('.container');
+ 
+getMovies(APIURL);
+
+async function getMovies(url){
+    
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    console.log(respData.results);
+
+    showMovies(respData.results);
+
+    return respData;
+    
+
+}
+
+function showMovies(movies){
+
+    container.innerHTML = ``;
+
+    movies.forEach((movie)=>{
+
+
+        const {
+            overview,
+            poster_path,
+            release_date,
+            title,
+            vote_average
+        } = movie;
+
+        const poster = document.createElement('div');
+
+        poster.innerHTML = `
+        
+        <img src="${IMGPATH + poster_path}" alt="" class="img">
+        <div class="title-container">
+            <h2 class="title">${title}</h2>
+            <span class="grade">${vote_average}</span>
+        
+        `
+        poster.classList.add('movie')
+
+        container.appendChild(poster);
+
+    })
+    
+}
+
+
