6606b511275f58cb10319e9952eff0830bf2d443
Adam Watchmaker <watchmaker1337@gmail.com>
2022-03-05 18:19:16 +0100

    show heart in click location

--- 29.Double Like/script.js
+++ 29.Double Like/script.js
@@ -3,6 +3,26 @@ const img = document.getElementById("img");
 
 let counter = 0;
 
-img.addEventListener("dblclick", () => {
+img.addEventListener("dblclick", (e) => {
+	const heart = document.createElement("i");
+	heart.classList.add("fa-solid");
+	heart.classList.add("fa-heart");
+
+	const x = e.clientX;
+	const y = e.clientY;
+
+	const leftOffset = e.target.offsetLeft;
+	const toptOffset = e.target.offsetTop;
+
+	const xInside = x - leftOffset;
+	const yInside = y - toptOffset;
+
+	heart.style.top = `${yInside}px`;
+	heart.style.left = `${xInside}px`;
+
+	img.appendChild(heart);
+
 	counterText.innerHTML = `You liked it ${counter++} times`;
+
+	setTimeout(() => heart.remove(), 1000);
 });
