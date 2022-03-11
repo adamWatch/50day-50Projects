513349242ad206d491d054e92ae191469a31a8ea
Adam Watchmaker <watchmaker1337@gmail.com>
2022-03-11 12:26:30 +0100

    add change color event

--- 36.Hoverboard/script.js
+++ 36.Hoverboard/script.js
@@ -2,15 +2,45 @@ const container = document.getElementById("container");
 console.log(container);
 
 function renderSquares() {
-	// const squares = [];
-
 	for (let i = 0; i < 256; i++) {
 		const div = document.createElement("div");
 		div.classList.add("square");
-		//squares.push('div');
 
 		container.appendChild(div);
 	}
 }
 
 renderSquares();
+
+const squares = document.querySelectorAll(".square");
+const colors = [
+	"green",
+	"blue",
+	"purple",
+	"red",
+	"yellow",
+	"indigo",
+	"royalblue",
+	"crimson",
+	"violet",
+	"orange",
+	"azure",
+	"khaki",
+	"brown",
+	"golden",
+];
+
+function getRandomColor() {
+	return colors[Math.floor(Math.random() * colors.length)];
+}
+
+squares.forEach((square) =>
+	square.addEventListener("mouseenter", (e) => {
+		e.target.style.backgroundColor = getRandomColor();
+		console.log("dziala");
+
+		setTimeout(() => {
+			e.target.style.backgroundColor = "";
+		}, 1000);
+	})
+);
