1d1f69abca8e4802edd997075f467c0512cc1a40
Adam Watchmaker <watchmaker1337@gmail.com>
2022-03-16 12:03:40 +0100

    add function

---
+++ 41.Verify Account/script.js
@@ -0,0 +1,14 @@
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, id) => {
+	code.addEventListener("keydown", (e) => {
+		if (e.key >= 0 && e.key <= 9) {
+			codes[id].value = "";
+			setTimeout(() => codes[id + 1].focus(), 10);
+		} else if (e.key === "Backspace") {
+			setTimeout(() => codes[id - 1].focus(), 10);
+		}
+	});
+});
